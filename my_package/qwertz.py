9d3c636da5d86c5fd058d663f5248c0002aa7931
Jonas Scharpf <jonas@brainelectronics.de>
2022-10-28 08:55:31 +0200

    add example source code my_package

---
+++ my_package/qwertz.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+QWERTZ
+
+Some description
+"""
+
+from random import sample
+from typing import List
+
+
+class QWERTZ(object):
+    """docstring for QWERTZ"""
+    def __init__(self, arg):
+        print('QWERTZ init with {}'.format(arg))
+
+    def print_okay(self):
+        """Print 'okay'"""
+        print("okay")
+
+    def get_list_of_int(self, how_long: int) -> List[int]:
+        random_int_list = sample(range(0, 100), how_long)
+
+        print('Generated random int list of length {}'.
+              format(len(random_int_list)))
+
+        return random_int_list
