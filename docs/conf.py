9d403992b5005dc5d86aa9a2a31dc370e1c1fc81
bbaranoff <bastienbaranoff@hotmail.fr>
2025-06-03 17:37:06 +0200

    commit

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ import shlex
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.doctest']
-
+extensions = ["sphinx_rtd_dark_mode"]
+default_dark_mode = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -103,10 +104,6 @@ todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
-html_context = {
-   # ...
-   "default_mode": "dark"
-}
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -115,10 +112,6 @@ html_theme = 'classic'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html[data-theme="dark"] {
-background-color: black;
-}
-html_theme_options = {"dark"}
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
