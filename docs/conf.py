91296225527472806e168a19a6f61cb93cce63cb
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-06-04 06:48:46 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,10 +29,6 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.doctest']
-extensions = ["sphinx_rtd_dark_mode"]
-extensions = ["sphinxcontrib.mermaid"]
-default_dark_mode = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -108,13 +104,16 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+# Add any paths that con
+html_theme = 'sphinx_rtd_theme'
+extensions = ['sphinx.ext.doctest']
+extensions = ["sphinx_rtd_dark_mode"]
+extensions = ["sphinxcontrib.mermaid"]
+default_dark_mode = True
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
